22138e34c669e78ee7f596f2be58669d66bfbc93
xavala <111553061+santiagodevweb@users.noreply.github.com>
2022-10-09 23:07:45 -0600

    app de periodico
    informacion de lo mas extravagante del estado de sinaloa

---
+++ app/Views/templates/header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>tuto de codeigniter</title>
+</head>
+<body>
+    <p>header->encabezado</p>
+    <h1><?php $title;?></h1>
+</body>
+</html>
